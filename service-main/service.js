2a567a19ef82f65ffe8c4cf56017ea45e56c431b
Zi Han Meng <zi.han.meng100@gmail.com>
2023-05-01 13:56:16 -0400

    Commit all .js files
    * Removed gitignore rule that excludes all .js files
    * Committed all .js files

---
+++ service-main/service.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+
+const host = process.env.MONGO_HOST ?? "localhost:27017";
+const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${host}`;
+const client = new MongoClient(url);
+
+app.use("*", (req, res, next) => {
+  console.log(req.path);
+  next();
+});
+
+app.use(express.json());
+
+app.get("/entries", async (req, res, next) => {
+  await client.connect();
+  const db = client.db("entries");
+  const collection = db.collection("entries");
+  const cursor = collection.find();
+  const entries = await cursor.toArray();
+  client.close();
+  res.json({ entries });
+});
+
+app.post("/entries", async (req, res, next) => {
+  await client.connect();
+  const db = client.db("entries");
+  const collection = db.collection("entries");
+  const entry = req.body;
+  console.log(entry);
+  try {
+    const result = await collection.insertOne(entry);
+    res.status(201).json({
+      message: "Entry inserted successfully",
+      insertedId: result.insertedId,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("*", (req, res, next) => {
+  res.sendFile("index.html", {
+    root: ".",
+  });
+});
+
+console.log(url);
+app.listen(5000, () => console.log("service-main listening on port 5000"));
